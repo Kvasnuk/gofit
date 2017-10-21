ca32a47b148773f2d39bfd610ccdc00908f66ba6
Kvasnuk <kvasnuk.vasiliy@gmail.com>
2017-10-21 21:11:01 +0300

    home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,98 +1,46 @@
 /*-----------------HOME PAGE MAP INIT----------------------------*/
-var gyms = [
-    {
-        lat: 50.447914,
-        lng:30.622192,
-        title: 'Banda1',
-        description: "<p>Banda1 text ...</p>",
-        link: "#",
-        rating: 2
-    },
-    {
-        lat: 50.417914,
-        lng:30.522192,
-        title: 'Banda2',
-        description: "<p>Banda2 text ...</p>",
-        link: "#",
-        rating: 4
-    },
-    {
-        lat:  50.467914,
-        lng:30.422192,
-        title: 'Banda3',
-        description: "<p>Banda3 text ...</p>",
-        link: "#",
-        rating: 2
-    },
-    {
-        lat:  50.447914,
-        lng:30.472192,
-        title: 'Banda4',
-        description: "<p>Banda4 text ...</p>",
-        link: "#",
-        rating: 2
-    },
-    {
-        lat:  50.447914,
-        lng:30.529192,
-        title: 'Banda5',
-        description: "<p>Banda5 text ... </p>",
-        link: "#",
-        rating: 2
-    },
-];
-function initMap() {
-    var image = {
-        url: '/assets/media/img/map-marker.svg',
-    };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: {lat: 50.447914, lng: 30.522192}
-    });
-    var infowindow = new google.maps.InfoWindow({
-        content: ''
-    });
-    for (var i = 0; i <  gyms.length; i++) {
-        var  gym = gyms[i];
-        var marker = new google.maps.Marker({
-            position: {lat: gym.lat, lng: gym.lng},
-            map: map,
-            icon: image,
-            id: 'gym-' + i,
-            title: gym.title,
-            description: gym.description,
-            link: gym.link,
-            rating: gym.rating,
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.close();
-            infowindow.setContent("<div class='gym-info'><h3>"+ this.title+"</h3><p>"+ this.rating +"</p> <p>"+ this.description +"</p><a href='"+ this.link +"'>></a></div>");
-            infowindow.open(map, this);
-        });
-    }
-}
+
 /*----------------------------END----------------------------*/
 
 $(document).ready(function () {
+    getVideoSize();
     bannerGreenBlockDraw();
     /*-----------------ADD HOME PAGE VIDEO----------------------------*/
-    (function(){
-        var windowWidth = $(window).width(),
-        videoHeight = (windowWidth /16) * 9;
-        $('#video__background').before(' <iframe width="'+ windowWidth +'" height="'+ videoHeight +'" src="https://www.youtube.com/embed/4B5sfE1fjLs?autoplay=1&mute=1&loop=1&playlist=4B5sfE1fjLs&controls=0" frameborder="0" allowfullscreen></iframe>');
-    })();
-    /*----------------END----------------------------*/
 
+    function getVideoSize(){
+        var windowWidth = $(window).width(),
+                 videoHeight = (windowWidth /16) * 9;
+        $('.video__description, .video__wrapper').height( videoHeight );
+        if($('#gofit-banner-video').length == 0){
+            initVideoBanner(windowWidth,videoHeight);
+        }else{
+            $('#gofit-banner-video').width(windowWidth);
+            $('#gofit-banner-video').height(videoHeight);
+        }
+    }
+    function initVideoBanner(videoWidth,videoHeight){
+        $('#video__background').before(' <iframe id="gofit-banner-video" width="'+ videoWidth +'" height="'+ videoHeight +'" src="https://www.youtube.com/embed/4B5sfE1fjLs?autoplay=1&mute=1&loop=1&playlist=4B5sfE1fjLs&controls=0" frameborder="0" allowfullscreen></iframe>');
+    }
+
+    /*----------------END----------------------------*/
+$(window).resize(function(){
+    getVideoSize();
+    bannerGreenBlockDraw();
+});
     function bannerGreenBlockDraw() {
+        // var  img = document.createElement('img');
+        // img.src = "/assets/media/img/patt-test.png";
         var banner = document.getElementById('video__background'),
             bannerWidth = $(window).width() / 2,
-            bannerHeight = $('.video__wrapper').innerHeight(),
+            bannerHeight =1 + ($(window).width() /16) * 9 ,
             bannerCurveTop = (bannerHeight / 2) - 55,
             bannerCurveBottom = (bannerHeight / 2) + 55,
             bannerCurvePoint = bannerWidth - 77,
             ctx = banner.getContext('2d');
         banner.width = bannerWidth;
         banner.height = bannerHeight;
+        ctx.clearRect(0, 0,  banner.width,  banner.height );
+        // var pat=ctx.createPattern(img,"repeat");
         ctx.beginPath();
         ctx.moveTo(0, 0);
         ctx.lineTo(bannerWidth, 0);
